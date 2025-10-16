8db0edc76284a5658e7d267fe58790518bd8128a
Ben Moskovitch <bemoskov@akamai.com>
2025-10-16 16:44:14 +0300

    Correcting error message

--- jarvis/__main__.py
+++ jarvis/__main__.py
@@ -1,3 +1,8 @@
-from auto_etp.jarvis import main_new_window
+import sys
+try:
+    from auto_etp.jarvis import main_new_window
 
-main_new_window()
+    main_new_window()
+except ModuleNotFoundError as ex:
+    print(ex)
+    print(f'Please run "{sys.executable} -m pybenjarvis.install" before attempting to run jarvis again')
